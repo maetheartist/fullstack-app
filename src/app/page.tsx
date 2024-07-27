614acecda80f6ac22a8778915256b889d55bb999
maetheartist <mariamkosemani1@gmail.com>
2024-07-27 01:53:53 +0100

    chore:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import SignIn from "./component/sign-in/SignIn";
-import SignUp from "./component/sign-up/SignUp";
-// import Authentication from "./component/auth/Authentication";
+
+
 
 export default function page() {
   return (
     <div>
     <SignIn/>
-    <SignUp/>
+  
     </div>
   );
 }
